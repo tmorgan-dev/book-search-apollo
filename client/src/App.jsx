c1879d4ae475189fa7142ed95034c4326d75936b
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-11-17 16:38:49 -0700

    connected client to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,37 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from"@apollo/client/link/context"
 
+
+
+import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri:"/graphql"
+})
+const authLink = setContext((_,{headers}) => {
+  const token = localStorage.getItem("id_token")
+  return {
+    headers: {
+      ...headers,
+      authorization: token?`bearer ${token}`:""
+    }
+  }
+})
 const client = new ApolloClient({
-  uri: '/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
+      </ApolloProvider>
     </>
   );
 }
