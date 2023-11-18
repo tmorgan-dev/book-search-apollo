3cd7c46718ad1b87ab1219b248400401f3940d01
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-11-17 17:03:01 -0700

    added logic to create users and login

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,8 +9,10 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,6 +22,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -36,7 +39,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      );;
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -72,13 +77,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
+      console.log(data)
+      console.log(savedBookIds);
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,66 +1,66 @@
-const {User} = require("../models")
-const {signToken, AuthenticationError} = require("../utils/auth")
+const { User } = require("../models")
+const { signToken, AuthenticationError } = require("../utils/auth")
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-              const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-      
-              return userData;
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+                return userData;
             }
-      
+
             throw AuthenticationError;
-          },
+        },
     },
     Mutation: {
-      addUser: async (parent, args) => {
+        addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
-      
+
             return { token, user };
-          },
-          login: async (parent, { email, password }) => {
+        },
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
-      
+
             if (!user) {
-              throw AuthenticationError;
+                throw AuthenticationError;
             }
-      
+
             const correctPw = await user.isCorrectPassword(password);
-      
+
             if (!correctPw) {
-              throw AuthenticationError;
+                throw AuthenticationError;
             }
-      
+
             const token = signToken(user);
             return { token, user };
-          },
-          saveBook: async (parent, { bookData }, context) => {
+        },
+        saveBook: async (parent, { bookData }, context) => {
             if (context.user) {
-              const updatedUser = await User.findByIdAndUpdate(
-                { _id: context.user._id },
-                { $push: { savedBooks: bookData } },
-                { new: true }
-              );
-      
-              return updatedUser;
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+
+                return updatedUser;
             }
-      
+
             throw AuthenticationError;
-          },
-          removeBook: async (parent, { bookId }, context) => {
+        },
+        removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
-              const updatedUser = await User.findOneAndUpdate(
-                { _id: context.user._id },
-                { $pull: { savedBooks: { bookId } } },
-                { new: true }
-              );
-      
-              return updatedUser;
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+
+                return updatedUser;
             }
-      
+
             throw AuthenticationError;
-          },
+        },
     },
 }
 
