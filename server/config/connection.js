b9591ff8f1f66809e38a50b05a16ccbad05b3e3c
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-11-28 12:05:18 -0700

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,37 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// Use the MONGODB_URL environment variable if available, otherwise use a default local URI
+const dbURI = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/googlebooks';
 
-module.exports = mongoose.connection;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+// Event listeners for Mongoose connection
+db.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+
+db.on('error', (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+});
+
+db.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+// Close Mongoose connection when the Node process exits
+process.on('SIGINT', () => {
+  db.close(() => {
+    console.log('Mongoose connection closed through app termination');
+    process.exit(0);
+  });
+});
+
+module.exports = db;
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+// module.exports = mongoose.connection;
