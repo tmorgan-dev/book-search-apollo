24d791c6b19b467f55faf2f9368e86ced9eebe4e
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-11-28 09:54:49 -0700

    updated port settings in server.js

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ const startApolloServer = async () => {
   }
 
   db.once('open', () => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
